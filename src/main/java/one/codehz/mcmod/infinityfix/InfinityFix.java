72fd7dd98177a91be918955497a12fb24747a5c2
solonovamax <solonovamax@12oclockpoint.com>
2022-10-07 14:46:51 -0400

    Update to 1.19.2 + more
    - Update to 1.19.2
    - Spectral arrows & tipped arrows have a 1/4 to 3/4 chance of being returned to the player
    - Use more optimal mixin targets
    - Update license to MIT in fabric.mod.json

--- src/main/java/one/codehz/mcmod/infinityfix/InfinityFix.java
+++ src/main/java/one/codehz/mcmod/infinityfix/InfinityFix.java
@@ -1,10 +1,12 @@
 package one.codehz.mcmod.infinityfix;
 
+
 import net.fabricmc.api.ModInitializer;
 
+
 public class InfinityFix implements ModInitializer {
-  @Override
-  public void onInitialize() {
-    // TODO
-  }
+    @Override
+    public void onInitialize() {
+        // TODO
+    }
 }
